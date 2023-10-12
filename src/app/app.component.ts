7ae5a840884837bc5fa90eb93051fef27830e6c4
jmayereup <jmayereup@gmail.com>
2023-10-12 17:34:13 +0700

    check mime type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FileUploaderComponent } from "./file-uploader/file-uploader.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    imports: [CommonModule, FileUploaderComponent]
 })
 export class AppComponent {
   title = 'file-uploader-demo';
---
+++ src/app/file-uploader/file-uploader.component.ts
@@ -0,0 +1,60 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-file-uploader',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './file-uploader.component.html',
+  styleUrls: ['./file-uploader.component.css']
+})
+
+
+export class FileUploaderComponent {
+  http = inject(HttpClient);
+
+  errorMessage : any = null;
+  successMessage = "";
+  selectedFile: File | undefined = undefined;
+
+  form = new FormGroup({
+    file: new FormControl()
+  });
+
+  constructor() { }
+
+  onFileSelected(event: any) {
+    this.successMessage = "";
+    this.errorMessage = null;
+    if (event.target.files.length > 0) {
+      this.selectedFile = event.target.files[0];
+      };
+    }
+  
+  onSubmit() {
+    const formData = new FormData();
+    formData.append('file', this.selectedFile!);
+
+    this.http.post('http://localhost:3001/upload', formData)
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+          this.selectedFile = undefined;
+          this.form.reset();
+          this.successMessage = "File Upload Successful";
+        },
+        error: (error) => {
+          console.error('Error Uploading File', error);
+          this.errorMessage = error;
+        }
+  });
+  }
+
+  clearMessages() {
+    this.errorMessage = null;
+    this.successMessage = "";
+  }
+
+}
